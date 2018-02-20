60847b9cc6b786656a7344c08cad41d148481873
christian westenfelder <c.westenfelder@outlook.com>
2018-02-20 18:56:11 +1100

    Initial Commit

---
+++ ST2_Tute_1/src/IntroToAlgorithms.java
@@ -0,0 +1,9 @@
+//Christian Westenfelder
+//20/02/2018 | University of Canberra
+
+public class IntroToAlgorithms {
+	
+	public static void main(String []args) {
+		System.out.println("Intro to Algorithms");
+	}
+}

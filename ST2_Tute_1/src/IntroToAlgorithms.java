8a673dcecf95d6ec1e74ab0539649b7ad0edce03
Unknown <c.westenfelder@outlook.com>
2018-02-21 11:43:06 +1100

    Q 1 Update

--- ST2_Tute_1/src/IntroToAlgorithms.java
+++ ST2_Tute_1/src/IntroToAlgorithms.java
@@ -1,9 +1,21 @@
 //Christian Westenfelder
 //20/02/2018 | University of Canberra
+//Initial Introduction Week 1 Tutorial
+
+import java.util.*;
 
 public class IntroToAlgorithms {
 	
 	public static void main(String []args) {
 		System.out.println("Intro to Algorithms");
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please insert the first number: "); int x = in.nextInt();
+		System.out.print("Please insert the second number: "); int y = in.nextInt();
+		System.out.print("The Sum of the two numbers is = "); int z = sum(x, y);
+		System.out.println(z);
+	}
+	
+	static int sum(int a, int b){
+		return (a + b);
 	}
 }
---
+++ ST2_Tute_1/src/IntroToAlgorithmsContinued.java
@@ -0,0 +1,9 @@
+//Christian Westenfelder
+//20/02/2018 | University of Canberra
+//Initial Introduction to Algorithms
+
+import java.util.*;
+
+public class IntroToAlgorithmsContinued {
+
+}
